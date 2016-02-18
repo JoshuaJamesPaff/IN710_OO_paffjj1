83004b2a05f0043b740fbb2a0ae0258df3ba0335
JoshuaJamesPaff <joshuapaff@hotmail.com>
2016-02-18 16:55:07 +1300

    Initial commit

---
+++ Practical 1 - Joshua Paff/Practical 1 - Joshua Paff/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Practical_1___Joshua_Paff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(736, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(358, 191);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Alter Dimensions";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 55;
+            this.listBox1.Location = new System.Drawing.Point(146, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(410, 499);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 701);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Practical 1 - Joshua Paff/Practical 1 - Joshua Paff/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical_1___Joshua_Paff
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Add("a,b");
+            listBox1.Items.Add("c,d");
+            listBox1.Items.Add("e,f");
+            listBox1.Items.Add("g,h");
+        }
+
+        public void TwoToOne()
+        {
+            const int columnCount = 4;
+            const int rowCount = 2;
+
+            Char[] oneDimensional = new Char[columnCount * rowCount];
+            Char[,] twoDimensional = new Char[columnCount, rowCount] { { 'a', 'b' }, { 'c', 'd' }, { 'e', 'f' }, { 'g', 'h' } };
+            
+            // convert to one dimension
+            for (int c = 0; c < columnCount; c++)
+                for (int r = 0; r < rowCount; r++)
+                {
+                    oneDimensional[(c * 2) + r] = twoDimensional[c,r];
+                }
+
+            //printing one dimensional array
+            for (int i = 0; i < oneDimensional.Length; i++)
+                listBox1.Items.Add(oneDimensional[i]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            TwoToOne();
+        }
+
+
+
+
+    }
+}
