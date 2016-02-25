2724cc73bc03cb371182b3e3a2d6b1e37faccfa0
JoshuaJamesPaff <joshuapaff@hotmail.com>
2016-02-26 04:36:47 +1300

    Practical 2 - Interfaces
    Prac 2 complete

---
+++ Prac2 - InterfacePractical - Joshua Paff/TestVS2015/Hand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TestVS2015
+{
+    public class Hand : IComparable<Hand>
+    {
+        public List<Card> CardsInHand { get; set; }
+        public int TotalHCP
+        {
+            get; set;
+        }
+
+        public Hand()
+        {
+            CardsInHand = new List<Card>();
+        }
+
+        //implementation of hand sort method based on HCP
+        public int CompareTo(Hand that)
+        {
+            if (this.TotalHCP > that.TotalHCP) return -1;
+            if (this.TotalHCP == that.TotalHCP) return 0;
+            return 1;
+        }
+
+        //allows hand sort to be called
+        public void SortHand()
+        {
+            CardsInHand.Sort();
+        }
+
+        public void AddCard(Card card)
+        {
+            CardsInHand.Add(card);
+        }
+
+        public int ComputeHCP()
+        {
+            TotalHCP = 0;
+            foreach (Card c in CardsInHand)
+                TotalHCP += c.HCP;
+
+            return 0;
+        }
+    }
+}
