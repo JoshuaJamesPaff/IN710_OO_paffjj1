6ace34c094c5b75ea1ee0934077fa47d3be38ff3
JoshuaJamesPaff <joshuapaff@hotmail.com>
2016-03-08 02:00:31 +1300

    Stacks Joshua Paff
    with 12 unit tests all working

---
+++ Stacks Joshua Paff/Stacks Joshua Paff/Stack.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_Joshua_Paff
+{
+    public class Stack
+    {
+        //linked list of strings to hold stack values
+        private LinkedList<String> stack;
+
+        // constructor initialises linked list
+        public Stack()
+        {
+            stack = new LinkedList<String>();
+        }
+
+        //adds newString to the top of the stack
+        public void Push(String newString)
+        {
+            stack.AddFirst(newString);
+        }
+
+        // removes and returns first string from stack
+        public String Pop()
+        {
+            if (stack.Count() > 0 )
+            {
+                String firstString = stack.First();
+                stack.RemoveFirst();
+                return firstString;
+            }
+            else
+            {
+                throw new System.ArgumentNullException("There are no Strings in the Stack pop.");
+            }
+        }
+
+        // returns first string on stack, returns exception if empty
+        public String Peek()
+        {
+            if (stack.Count() > 0)
+            {
+                return stack.First();
+            }
+            else
+            {
+                throw new System.InvalidOperationException("There are no Strings in the Stack to peek at.");
+            }
+
+        }
+
+        //returns number of items in stack
+        public int Count()
+        {
+            return stack.Count();
+          
+        }
+
+        //if stack has 0 elements returns true, else false
+        public bool IsEmpty()
+        {
+            if (stack.Count() == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Stacks Joshua Paff/UnitTestProjectStacks/UnitTest1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stacks_Joshua_Paff;
+
+
+namespace UnitTestProjectStacks
+{
+    [TestClass]
+    public class UnitTestStack
+    {
+        [TestMethod]
+        public void IsEmptyNewStack_ReturnsTrue()
+        {
+            Stack stack = new Stack();
+            bool expected = true;
+
+            bool actual = stack.IsEmpty();
+
+            Assert.AreEqual(expected, actual, "New Stack is not empty");
+        }
+
+        [TestMethod]
+        public void PushedStackIsEmpty_ReturnsFalse()
+        {
+            Stack stack = new Stack();
+            stack.Push("one");
+
+            bool expected = false;
+
+            bool actual = stack.IsEmpty();
+
+            Assert.AreEqual(expected, actual, "The pushed stack is empty");
+        }
+
+        [TestMethod]
+        public void IsEmptyPoppedStack_ReturnsTrue()
+        {
+            Stack stack = new Stack();
+            stack.Push("one");
+            stack.Pop();
+
+            bool expected = true;
+
+            bool actual = stack.IsEmpty();
+
+            Assert.AreEqual(expected, actual, "The popped stack is not empty");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.InvalidOperationException))]
+        public void PeekEmptyStack_ReturnsException()
+        {
+            Stack stack = new Stack();
+            stack.Peek();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PopEmptyStack_ReturnsException()
+        {
+            Stack stack = new Stack();
+            stack.Pop();
+        }
+
+        [TestMethod]
+        public void PushPop_ReturnsPushValue()
+        {
+            Stack stack = new Stack();
+
+            String pushed = "one";
+            stack.Push(pushed);
+            String popped = stack.Pop();
+
+            Assert.AreEqual(pushed, popped, "Popped value does not equal pushed value");
+           
+        }
+
+        [TestMethod]
+        public void PushPop_DeletesPopValue()
+        {
+            Stack stack = new Stack();
+
+            String pushed = "one";
+            stack.Push(pushed);
+            stack.Pop();
+            bool expectedBool = true;
+            bool actualBool = stack.IsEmpty();
+
+            Assert.AreEqual(expectedBool, actualBool, "Popped value not deleted");
+        }
+
+        [TestMethod]
+        public void PushPeek_ReturnsPushValue()
+        {
+            Stack stack = new Stack();
+
+            String pushed = "one";
+            stack.Push(pushed);
+            String peeked = stack.Peek();
+
+            Assert.AreEqual(pushed, peeked, "Pushed value does not equal peeked value");
+
+        }
+
+        [TestMethod]
+        public void PushPeek_RetainsPeekValue()
+        {
+            Stack stack = new Stack();
+
+            String pushed = "one";
+            stack.Push(pushed);
+            stack.Peek();
+            bool expectedBool = false;
+
+            bool actualBool = stack.IsEmpty();
+
+            Assert.AreEqual(expectedBool, actualBool, "Peeked value not on top of stack");
+        }
+
+        [TestMethod]
+        public void CountNewStack_ReturnsZero()
+        {
+            Stack stack = new Stack();
+
+            int expected = 0;
+            int actual = stack.Count();
+
+            Assert.AreEqual(expected, actual, "Count of new stack does not equal 0");
+        }
+
+        [TestMethod]
+        public void PushCount_ReturnsCountOfPushed()
+        {
+            Stack stack = new Stack();
+
+            String pushed1 = null;
+            String pushed2 = "";
+            String pushed3 = "three"; 
+            stack.Push(pushed1);
+            stack.Push(pushed2);
+            stack.Push(pushed3);
+
+            int expected = 3;
+            int actual = stack.Count();
+            
+            Assert.AreEqual(expected, actual, "Number of items pushed does not equal number of items counted");
+        }
+
+        [TestMethod]
+        public void PushPopCount_ReturnsZero()
+        {
+            Stack stack = new Stack();
+
+            String pushed1 = "one";
+           
+            stack.Push(pushed1);
+            stack.Pop();
+
+            int expected = 0;
+            int actual = stack.Count();
+
+            Assert.AreEqual(expected, actual, "Popped stack not empty");
+        }
+
+    }
+}
