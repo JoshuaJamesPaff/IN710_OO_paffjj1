42cd141f887d9e08efb2c9dd4941940ef2bf96ad
JoshuaJamesPaff <joshuapaff@hotmail.com>
2016-03-14 05:02:48 +1300

    Animal Shelter
    completed

---
+++ Animal Shelter/IN710 4.1 Animal Shelter Solution 2014/PictureDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace IN710_4._1_Animal_Shelter_Solution_2014
+{
+    class PictureDisplay: IDisplay
+    {
+        //holds picture box list 
+        private List<PictureBox> pictureBoxList;
+
+        public PictureDisplay(List<PictureBox> pictureBoxList)
+        {
+            this.pictureBoxList = pictureBoxList;
+        }
+
+        //takes a list of critters and sets their ImageFileNames to a picture box
+        public void displayCritterList(List<Critter> critterList)
+        {
+            for (int i = 0; i < critterList.Count; i++)
+            {
+                pictureBoxList[i].Image = Image.FromFile(critterList[i].ImageFileName);
+            }
+        }
+
+        public void clearDisplay()
+        {
+            for (int i = 0; i < pictureBoxList.Count; i++)
+            {
+                //sets picture boxes to null
+                pictureBoxList[i].Image = null;
+            }
+        }
+    }
+}
---
+++ Animal Shelter/IN710 4.1 Animal Shelter Solution 2014/TextDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IN710_4._1_Animal_Shelter_Solution_2014
+{
+    class TextDisplay: IDisplay
+    {
+        private ListBox listBox;
+
+        public TextDisplay(ListBox listBox)
+        {
+            this.listBox = listBox;
+        }
+    
+        public void displayCritterList(List<Critter> critterList)
+        {
+            foreach (Critter crit in critterList)
+            {
+                listBox.Items.Add(crit.ToString());
+            }
+        }
+
+        public void clearDisplay()
+        {
+            listBox.Items.Clear();
+        }
+    }
+}
