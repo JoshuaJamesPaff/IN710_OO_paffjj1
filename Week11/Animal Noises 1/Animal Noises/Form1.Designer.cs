ce3a683baeb86f5e955db1024aa5cb408b8e22d8
JoshuaJamesPaff <joshuapaff@hotmail.com>
2016-05-17 03:07:43 +1200

    animal noises part 1
    week 11.2 - 1

---
+++ Week11/Animal Noises 1/Animal Noises/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Animal_Noises
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.btnWhat = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(49, 42);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(157, 43);
+            this.btnSpeak.TabIndex = 0;
+            this.btnSpeak.Text = "Speak";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // btnWhat
+            // 
+            this.btnWhat.Location = new System.Drawing.Point(49, 91);
+            this.btnWhat.Name = "btnWhat";
+            this.btnWhat.Size = new System.Drawing.Size(157, 43);
+            this.btnWhat.TabIndex = 1;
+            this.btnWhat.Text = "What Is It?";
+            this.btnWhat.UseVisualStyleBackColor = true;
+            this.btnWhat.Click += new System.EventHandler(this.btnWhat_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(49, 140);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(157, 42);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(253, 234);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.btnWhat);
+            this.Controls.Add(this.btnSpeak);
+            this.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.Button btnWhat;
+        private System.Windows.Forms.Button buttonStop;
+
+    }
+}
+
---
+++ Week11/Animal Noises 1/Animal Noises/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Animal_Noises
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //animal + thread object
+        private Animal mainAnimal;
+        private Thread speakThread;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mainAnimal = new Animal();
+        }
+
+        //calls animal.speak in new thread
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            speakThread = new Thread(mainAnimal.speak);
+            speakThread.Start();
+        }
+
+        private void btnWhat_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("It is a frog");
+        }
+
+        //aborts animal.speak when stop clicked
+        private void buttonStop_Click(object sender, EventArgs e)
+        {   
+            speakThread.Abort();
+        }
+
+        //aborts animal.speak when form close clicked
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            speakThread.Abort();
+        }
+    }
+}
